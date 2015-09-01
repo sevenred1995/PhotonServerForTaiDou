02bd1db42eee3644c9c752c8e48507c855e80700
lqh <852215684@qq.com>
2015-09-01 09:03:51 +0800

    Signed-off-by: lqh <852215684@qq.com>

---
+++ TaiDouServer/ClientPeer.cs
@@ -0,0 +1,29 @@
+﻿using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiDouServer
+{
+    class ClientPeer:PeerBase
+    {
+        public ClientPeer(IRpcProtocol protocol,IPhotonPeer unmanagedPeer)
+            :base(protocol,unmanagedPeer)
+        {
+
+        }
+        protected override void OnDisconnect(PhotonHostRuntimeInterfaces.DisconnectReason reasonCode, string reasonDetail)
+        {
+            
+        }
+
+        //客户端发起请求时调用
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+          
+        }
+    }
+}
---
+++ TaiDouServer/TaiDouApplication.cs
@@ -0,0 +1,29 @@
+﻿using Photon.SocketServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TaiDouServer
+{
+    class TaiDouApplication:ApplicationBase
+    {
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest.Protocol, initRequest.PhotonPeer);
+        }
+
+        protected override void Setup()
+        {
+           
+        }
+
+        protected override void TearDown()
+        {
+            
+        }
+    }
+}
